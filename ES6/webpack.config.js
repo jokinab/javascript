78672c41a37e85a0be11e9afa8ecbb0615a1c6b3
jokinab <jarnalbilbao@gmail.com>
2017-07-29 17:54:28 +0200

    Corrección configuración de webpack

--- ES6/webpack.config.js
+++ ES6/webpack.config.js
@@ -1,48 +1,32 @@
 var path = require('path');
-
- module.exports = {
-     entry: './src/es_01.js',
-     output: {
-         path: path.resolve(__dirname, 'dist'),
-         filename: 'bundle.js'
-     },
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 },
-                 exclude: /node_modules/   
-             }
-         ]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
- };
-
-
-/*
-var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: path.resolve(__dirname, '/src/es_01.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-        test: /\.js$/,
-        loaders: ['babel'],
-        exclude: /node_modules/
-    }]
-  },
-  resolve: {
-    extensions: [".js"]
-  }
+    entry: path.join(__dirname, 'src'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                exclude: /node_modules/   
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
 };
-*/
