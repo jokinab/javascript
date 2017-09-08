3c75af0ea23486f0c252dd8e9b4cd3c358a46690
Jokin Arnal <jokin@coexia.com>
2017-09-08 16:51:36 +0200

    boton 00

--- reactJS/c_0_presentacion/app-presentacion/src/App.js
+++ reactJS/c_0_presentacion/app-presentacion/src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+class Titulo extends Component {
+  
+  render() {
+    return (
+      <h1>{this.props.mensaje}</h1>
+    );  
+  }
+}
+
 class App extends Component {
+
+  constructor (...args) {
+    super(...args);
+    this.state = {  mensaje: "Mensaje Inicial!" };
+  }  
+  
+  cambiarTitulo() {
+    this.setState( { mensaje : "Mensaje cambiado!" } ) ;
+  }
+  
   render() {
     return (
       <div className="App">
-        <h1>Hola mundo</h1>
+        <button onClick={ this.cambiarTitulo.bind(this) } >Cambiar el titulo</button>
+        <Titulo mensaje = { this.state.mensaje } />
       </div>
     );
   }
