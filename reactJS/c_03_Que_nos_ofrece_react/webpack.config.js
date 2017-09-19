26ae45225572ded77a7d0a0504d336a7aa3ac6b8
Jokin Arnal <jokin@coexia.com>
2017-09-19 16:43:54 +0200

    Ciclo de vida de los componentes

---
+++ reactJS/c_03_Que_nos_ofrece_react/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    }
+}
+
