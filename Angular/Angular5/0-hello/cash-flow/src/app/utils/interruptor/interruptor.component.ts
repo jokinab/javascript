8d953db496eb82a317149d965503b555bec54e3b
jokinab <jarnalbilbao@gmail.com>
2018-03-24 13:48:30 +0100

    direciva *ng-If

--- Angular/Angular5/0-hello/cash-flow/src/app/utils/interruptor/interruptor.component.ts
+++ Angular/Angular5/0-hello/cash-flow/src/app/utils/interruptor/interruptor.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterruptorComponent implements OnInit {
 
+  activo = true;
+
+  intercambiar() {
+    this.activo = !this.activo;
+  }
+
   constructor() { }
 
   ngOnInit() {
