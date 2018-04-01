68a73e7caf46d59ff93269821b38581f7d6e2662
jokinab <jarnalbilbao@gmail.com>
2018-04-01 14:04:31 +0200

    Servicios Inyectables

---
+++ Angular/Angular5/5-Inyeccion-de-servicios/cash-flow/src/app/lib/components/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TitleComponent } from './title.component';
+
+@Component({
+  selector: 'cf-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/Angular5/5-Inyeccion-de-servicios/cash-flow/src/app/views/operations/operations.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import { Operation } from './operation';
+
+@Injectable()
+export class OperationsService {
+
+  private operations: Operation[] = [];
+
+  constructor() {}
+
+  public getNumberOfOperations(): number {
+    return this.operations.length;
+  }
+  public getOperationsList(): Operation[] {
+    return this.operations;
+  }
+  public getOperationById(id: string): Operation {
+    return this.operations.find(element => element._id === id);
+  }
+  public saveOperation(operation: Operation) {
+    operation._id = new Date().getTime().toString();
+    this.operations = [...this.operations, operation];
+  }
+  public deleteOperation(operation: Operation) {
+    const index = this.operations.indexOf(operation);
+    this.operations.splice(index, 1);
+  }
+
+}
