0c9760d6cc1f26f885cafcea859364aa9e252f2c
Jokin Arnal <jokin@coexia.com>
2018-04-05 12:13:29 +0200

    Interceptores

--- Angular/Angular5/6-Comunicaciones_http/cash-flow/src/app/app.module.ts
+++ Angular/Angular5/6-Comunicaciones_http/cash-flow/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, transition } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -8,6 +8,10 @@ import { ComponentsModule } from './lib/components/components.module';
 
 import { HomeModule } from './views/home/home.module';
 import { NotFoundModule } from './views/not-found/not-found.module';
+
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http/src/client";
+import { CatchInterceptorService } from './lib/catch-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,16 @@ import { NotFoundModule } from './views/not-found/not-found.module';
     AppRoutingModule,
     ComponentsModule,
     HomeModule,
+    HttpClientModule,
     NotFoundModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:  HTTP_INTERCEPTORS,
+      useClass: CatchInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Angular/Angular5/6-Comunicaciones_http/cash-flow/src/app/lib/catch-interceptor.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse
+} from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { tap } from "rxjs/operators";
+
+@Injectable()
+export class CatchInterceptorService {
+
+  private started;
+
+  public intercept( req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+
+
+    const successCallback = this.interceptResponse.bind(this);
+    const errorCallback = this.catchError.bind(this);
+    const interceptionOperator = tap<HttpEvent<any>>( successCallback, errorCallback );
+    
+    this.started = Date.now();
+    const handledRequest = next.handle(req);
+    console.log('entra');
+    return handledRequest.pipe(interceptionOperator);
+  }
+
+  private interceptResponse(event: HttpEvent<any>) {
+    if (event instanceof HttpResponse) {
+      const elapsed_ms = Date.now() - this.started;
+      console.debug(`Request for ${event.url} took ${elapsed_ms} ms.`);
+    }
+  }
+
+  private catchError(err) {
+    if (err instanceof HttpErrorResponse) {
+      this.catchHttpError(err);
+    } else {
+      console.error(err.message);
+    }
+  }
+
+  private catchHttpError(err: HttpErrorResponse) {
+    if (err.status === 401) {
+      console.log("Not authorized");
+    } else {
+      console.warn(err.statusText);
+    }
+  }
+
+}
