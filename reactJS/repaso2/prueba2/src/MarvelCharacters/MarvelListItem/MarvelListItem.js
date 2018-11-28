feb6f04789fc91c898f8000188480ead3be751ba
Jokin Arnal <jokin@coexia.com>
2018-11-28 10:49:31 +0100

    favs

--- reactJS/repaso2/prueba2/src/MarvelCharacters/MarvelListItem/MarvelListItem.js
+++ reactJS/repaso2/prueba2/src/MarvelCharacters/MarvelListItem/MarvelListItem.js
@@ -12,6 +12,10 @@ export default class MarvelListItem extends Component {
         <div className="marvel-item-cnt">
           <h2 className="marvel-item-name"><Link to={`/character/${this.props.marvelItem.id}`}>{this.props.marvelItem.name}</Link></h2>
           <p className="marvel-item-desc">{this.props.marvelItem.description}</p>
+          <p className="actions-item">
+            <span className="rate-item">Rate this Marvel Item</span>
+            <span className="rate-item">Add Marvel Item to your Favorites</span>
+          </p>
         </div>
       </article>
     )
