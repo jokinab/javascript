de3038910d726c64b927810f26a52075a2162d95
jokinab <jarnalbilbao@gmail.com>
2018-12-08 19:43:05 +0100

    Prueba commit Atom

--- reactJS/repaso2/prueba2/src/ApiMarvel/ApiMarvel.js
+++ reactJS/repaso2/prueba2/src/ApiMarvel/ApiMarvel.js
@@ -4,7 +4,6 @@ const APIKEY_QUERYSTRING = 'apikey=1746232ad739a6d56e75f025d04655f9';
 
 export default class ApiMarvel {
 
-
     static getMarvelCharacters( offset ) {
       const url = `${API_URL}/characters?offset=${offset}&${APIKEY_QUERYSTRING}`;
       return  fetch(url);
