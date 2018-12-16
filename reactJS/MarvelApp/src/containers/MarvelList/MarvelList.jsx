459594cfae883bc007519219be8811e3c77cdbff
jokinab <jarnalbilbao@gmail.com>
2018-12-16 18:23:20 +0100

    Quitado constructor

--- reactJS/MarvelApp/src/containers/MarvelList/MarvelList.jsx
+++ reactJS/MarvelApp/src/containers/MarvelList/MarvelList.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchMarvelItems } from './../../actions/marvelList';
 
-
 import PropTypes from 'prop-types';
 
 import MarvelCard from './../../components/MarvelCard/MarvelCard';
@@ -12,40 +11,15 @@ import { IsLoading } from './../../components/IsLoading/IsLoading';
 
 
 class MarvelListComponent extends Component {
-  constructor(...args){
-    super(...args);
-    this.state = {
-      PageMarvel: this.props.match.params.PageMarvel,
-    }
-
-  }
-
-  static getDerivedStateFromProps(props, state){
-
-    if ( props.PageMarvel !== state.PageMarvel ) {
-
-      props.onFetchMarvelItems(props.PageMarvel);
-
-      return {
-        PageMarvel: props.PageMarvel
-      }
-    }else{
-      return null;
-    }
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
-    if(prevProps.PageMarvel !== this.props.PageMarvel){
-      //Perform some operation here
-      this.setState({PageMarvel: this.props.PageMarvel});
-    }
-
-  }
 
   componentDidMount() {
     this.props.onFetchMarvelItems(this.props.match.params.PageMarvel);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if ( this.props.PageMarvel !== prevProps.PageMarvel ) {
+        this.props.onFetchMarvelItems(this.props.PageMarvel);
+    }
   }
 
   render() {
@@ -53,18 +27,19 @@ class MarvelListComponent extends Component {
       <div className="todocontent">
         <div className="container-body">
 
+          <div className="container-pagination">
+            { ! this.props.isFetching && this.props.marvelItems.length > 0
+              && <ListPagination baseLink={'/characters/'}
+                                 currentPage={this.props.match.params.PageMarvel}
+                                 totalPages={Math.floor( parseInt(this.props.totalItems) / 20)} /> }
+          </div>
+
           <div className="cards-list">
             { this.props.isFetching && <IsLoading /> }
 
             { ! this.props.isFetching && this.props.marvelItems.length > 0
                 && this.props.marvelItems.map( (item, index) => <MarvelCard key={index} marvelItem={item} /> )
             }
-          </div>
-          <div className="container-pagination">
-            { ! this.props.isFetching && this.props.marvelItems.length > 0
-              && <ListPagination baseLink={'/characters/'}
-                                 currentPage={this.props.match.params.PageMarvel}
-                                 totalPages={Math.floor( parseInt(this.props.totalItems) / 20)} /> }
           </div>
 
         </div>
