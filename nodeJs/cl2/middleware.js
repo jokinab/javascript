0a1b10ae262b5c78d575bb6c0bb5e68c04b8738d
jokinab <jarnalbilbao@gmail.com>
2019-02-10 18:24:29 +0100

    Clase 2 inicio

---
+++ nodeJs/cl2/middleware.js
@@ -0,0 +1,13 @@
+'use strict'
+/** 
+ * Modulo de logica intermedia
+ * @module middleware
+*/
+/**
+ * @param {object} app - aplicacion de express
+ * @param {object} express - el propio framework express
+ * @return configura la aplicacion
+ */
+module.exports.useMiddleware = (app, express) => {
+  
+}
